906f5dbeeff2d6ee44ddcd38f6dc64dd008fecda
Fred Mitchell <fred.mitchell@gmx.de>
2017-09-03 12:16:02 +0200

    initial

---
+++ lib/media_indexer.ex
@@ -0,0 +1,18 @@
+defmodule MediaIndexer do
+  @moduledoc """
+  Documentation for MediaIndexer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MediaIndexer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/media_indexer_test.exs
@@ -0,0 +1,8 @@
+defmodule MediaIndexerTest do
+  use ExUnit.Case
+  doctest MediaIndexer
+
+  test "greets the world" do
+    assert MediaIndexer.hello() == :world
+  end
+end
