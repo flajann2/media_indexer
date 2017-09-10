3ae556500fa8dfed03c5fb569699662119882152
Fred Mitchell <fred.mitchell@gmx.de>
2017-09-10 21:43:36 +0200

    cli in the works

--- lib/media_indexer.ex
+++ lib/media_indexer.ex
@@ -8,7 +8,6 @@ defmodule MediaIndexer do
   
   def main(args) do
     args |> parse_args |> process
-    ap args
   end
 
   def process([]) do
@@ -16,7 +15,6 @@ defmodule MediaIndexer do
   end
 
   def process(options) do
-    puts "Hello #{options[:name]}"
     ap options
   end
 
@@ -28,7 +26,56 @@ defmodule MediaIndexer do
       author: "Fred mitchell fred.mitchell@gmx.de",
       about: "Scan all your media files and documents, index them for searching.",
       allow_unknown_args: false,
-      parse_double_dash: true,
+      parse_double_dash: false,
+      subcommands: [
+        scan: [
+          name: "scan",
+          about: "scan the given directories for content and metadata",
+          allow_unknown_args: true,
+          parse_double_dash: false,
+          args: [
+            cache: [
+              value_name: "CACHE_NAME",
+              help: "name of cache create",
+              required: true,
+              parser: :string,
+            ],            
+          ],
+        ],
+        search: [
+          name: "search",
+          about: "search the database for content",
+          allow_unknown_args: true,
+          parse_double_dash: false,
+          args: [],
+        ],
+        drop: [
+          name: "drop",
+          about: "drop the named media cache",
+          args: [
+            cache: [
+              value_name: "CACHE_NAME",
+              help: "name of cache to drop",
+              required: true,
+              parser: :string,
+            ],
+          ],
+        ],
+        list: [
+          name: "list",
+          about: "list the content of the media caches",
+          allow_unknown_args: true,
+          parse_double_dash: false,
+          args: [],
+        ],
+      ],
+      flags: [
+        verbosity: [
+          short: "-v",
+          help: "Verbosity level",
+          multiple: true,
+        ],
+      ]
     ) |> Optimus.parse!(args)
   end
 end
