328b8b548db2185abac7af47c264da88b540f78e
Fred Mitchell <fred.mitchell@gmx.de>
2017-09-10 10:55:27 +0200

    using optimus instead of options parser

--- lib/media_indexer.ex
+++ lib/media_indexer.ex
@@ -3,23 +3,32 @@ defmodule MediaIndexer do
   Documentation for MediaIndexer.
   """
 
+  import Apex
+  import IO
+  
   def main(args) do
     args |> parse_args |> process
+    ap args
   end
 
   def process([]) do
-    IO.puts "No arguments given"
+    puts "No arguments given"
   end
 
   def process(options) do
-    IO.puts "Hello #{options[:name]}"
+    puts "Hello #{options[:name]}"
+    ap options
   end
 
   defp parse_args(args) do
-    {options, _, _} = OptionParser.parse(args,
-      switches: [name: :string]
-    )
-    options
+    Optimus.new!(
+      name: "mediax",
+      description: "Media Indexer",
+      version: "0.0.1",
+      author: "Fred mitchell fred.mitchell@gmx.de",
+      about: "Scan all your media files and documents, index them for searching.",
+      allow_unknown_args: false,
+      parse_double_dash: true,
+    ) |> Optimus.parse!(args)
   end
-
 end
